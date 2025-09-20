2dafc8753b213e825f7f8f014b29959637bfd025
PavelSapegin <st140957@student.spbu.ru>
2025-09-20 11:01:27 +0300

    Fix advanced euclid's algorithm for negative numbers

--- src/python_tasks/task2/advanced_euclid.py
+++ src/python_tasks/task2/advanced_euclid.py
@@ -2,13 +2,14 @@ def advanced_euclid(a,b):
     if a % b == 0:
         return (b,0,1)
     
+    a,b = abs(a),abs(b)
     d,x0,y0 = advanced_euclid(a,a%b)
 
     x = y0
     y = x0 - (a//b)*y0
 
-    return (abs(d),x,y)
+    return f"{abs(d)} = {a}*{x} + {b}*{y}"
 
 
-
-print(advanced_euclid(30,12))
+#Example
+print(advanced_euclid(30,-12))
