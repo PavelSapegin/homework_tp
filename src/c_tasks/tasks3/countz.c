6193c374b878a64c32485d8d714d727eecbdb05f
PavelSapegin <pawsapegin@yandex.ru>
2025-10-05 21:38:15 +0300

    Realize a count zeros in array

---
+++ src/c_tasks/tasks3/countz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int 10;
+    int count = 0;
+    int arr[10] = {0,1,2,3,4,0,0,1,1,2};
+    for (int i = 0; i < n; ++i)
+    {
+        if (arr[i] == 0)
+            count++;
+    }
+
+    printf("Количество нулей в массиве: %d шт.\n", count);
+
+    return 0;
+}
+
+
+
+
+
+
