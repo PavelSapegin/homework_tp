165c6d9f301612a8ae985b408e33fd804043d19d
PavelSapegin <pawsapegin@yandex.ru>
2025-10-11 18:29:08 +0300

    Add user input, count of elements, which were change position and formating

--- src/c_tasks/tasks4/bubble_sort.c
+++ src/c_tasks/tasks4/bubble_sort.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
-void bubble_sort(int arr[],int len_arr)
+void bubble_sort(int arr[], int len_arr)
 {
 
     for (int i = 0; i < len_arr; ++i)
-    {  
+    {
         for (int j = i + 1; j < len_arr; ++j)
         {
             if (arr[i] > arr[j])
@@ -17,14 +16,35 @@ void bubble_sort(int arr[],int len_arr)
         }
     }
 }
+
 int main()
 {
-    int nums[] = {5,4,1,12,12};
-    int len_nums = sizeof(nums) / sizeof(nums[0]);
-    
-    
+    int nums[100];
+
+    // копия массива для подсчета элементов
+    // изменивших свою позицию
+    int nums_copy[100]; 
+    int len_nums = 0; // считаем длину массива с реальнымим данными
+    int count = 0;
+
+    while ((len_nums < 100) && (scanf("%d", &nums[len_nums]) == 1))
+    {
+        nums_copy[len_nums] = nums[len_nums];
+        len_nums++;
+        
+        char c = getchar();
+        if (c == '\n')
+            break;
+    }
+
     bubble_sort(nums, len_nums);
-    for (int i = 0; i < 5; ++ i)
-        printf("%d ", *(nums + i));
-    return 0;
+
+    
+    for (int i = 0; i < len_nums; ++i)
+    {
+        printf("%d ", nums[i]);
+        if (nums[i] != nums_copy[i])
+            count++;
+    }
+    return count;
 }
