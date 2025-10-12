f4b788fa17c2ca82d09ff06245afeb0fa677ac4e
PavelSapegin <pawsapegin@yandex.ru>
2025-10-12 19:38:21 +0300

    Realize my solution

---
+++ src/python_tasks/task4/my_sol_queens.py
@@ -0,0 +1,21 @@
+
+def my_queens(n,row=0, cols=set(), dia1=set(), dia2=set()):
+
+    if row == n:
+        return True
+
+    count = 0
+    for col in range(n):
+        if col not in cols \
+            and (row + col) not in dia1 \
+            and (row - col) not in dia2:
+            rownext = row + 1
+            colsnext = cols | {col}
+            dia1next = dia1 | {row + col}
+            dia2next = dia2 | {row - col}
+            count += my_queens(n,rownext, colsnext, dia1next, dia2next)
+
+    return count
+
+
+print(my_queens(13))
