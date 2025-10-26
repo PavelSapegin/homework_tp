550b2729d58e342a3b2b44f7ff773c793387a944
PavelSapegin <pawsapegin@yandex.ru>
2025-10-26 17:34:28 +0300

    Add user input

--- src/c_tasks/tasks6/sortedlist.c
+++ src/c_tasks/tasks6/sortedlist.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct list {
+typedef struct list
+{
     int data;
     struct list *next;
-}list;
+} list;
 
 void print(list **start)
 {
     list *curr = *start;
     while (curr != NULL)
     {
-        printf("%d\n",curr->data);
+        printf("%d\n", curr->data);
         curr = curr->next;
     }
 }
 
-void append(list **start,int new_val)
+void append(list **start, int new_val)
 {
     list *new = malloc(sizeof(list));
     new->data = new_val;
     new->next = NULL;
-    
+
     if (*start == NULL)
     {
-        *start = malloc(sizeof(list));
-        (*start)->data = new->data;
-        (*start)->next = new->next;
+        *start = new;
         return;
     }
 
     if (new->data < (*start)->data)
     {
-       int tmp_data = (*start)->data;
+        int tmp_data = (*start)->data;
 
         (*start)->data = new->data;
         new->data = tmp_data;
@@ -44,11 +43,9 @@ void append(list **start,int new_val)
     list *curr = *start;
     while (curr->next != NULL && curr->next->data < new->data)
         curr = curr->next;
-    
-        new->next = curr->next;
-        curr->next = new;
 
-
+    new->next = curr->next;
+    curr->next = new;
 }
 
 void delete(list **start, int val)
@@ -56,10 +53,11 @@ void delete(list **start, int val)
     if (*start == NULL)
         return;
 
-    
     while ((*start)->next != NULL && (*start)->data == val)
     {
+        list *del = *start;
         *start = (*start)->next;
+        free(del);
     }
 
     list *curr = *start;
@@ -67,22 +65,89 @@ void delete(list **start, int val)
     {
         if (curr->next->data == val)
         {
+            list *del = curr->next;
             curr->next = curr->next->next;
+            free(del);
         }
         else
             curr = curr->next;
     }
+}
 
+void spaces()
+{
+    for (int i = 0; i < 1000; ++i)
+        printf("             \n");
+}
+
+void menu()
+{
+    printf("Привет, выберите пункт для работы в отсортированном списке\n");
+    printf("0 - Выйти\n");
+    printf("1 - Добавить значение\n");
+    printf("2 - Удалить значение\n");
+    printf("3 - Распечатать список\n");
 }
 int main()
 {
     list *st = NULL;
+    int choice = -10;
 
-    append(&st,12);
-    append(&st,14);
-    append(&st,13);
-    append(&st,1);
-    append(&st,10000);
-    print(&st);
+    
+    while (1)
+    {
+
+        printf("\n");
+        menu();
+        if (scanf("%d",&choice) !=1 || choice < 0 || choice > 3)
+        {
+            spaces();
+            printf("Ошибка ввода");
+            continue;
+        }
+
+        switch(choice)
+        {
+            case 0:
+                return 0;
+            
+            case 1:
+            {
+                int num;
+                printf("Введите значение:\n");
+                if (scanf("%d",&num) == 1)
+                {
+                    append(&st,num);
+                    spaces();
+                    printf("Значение успешно добавлено!\n");
+                }
+                else
+                    {
+                    spaces();
+                    printf("Ошибка\n");
+                    }
+                break;
+            }
+            case 2:
+            {
+                int num;
+                printf("Введите значение:\n");
+                if (scanf("%d",&num) == 1)
+                {
+                    delete(&st,num);
+                    spaces();
+                    printf("Значение успешно удалено!");
+                }
+                
+                break;
+            }
+            case 3:
+            {
+                spaces();
+                print(&st);
+                break;
+            }
+        }
+    }
     return 0;
 }
