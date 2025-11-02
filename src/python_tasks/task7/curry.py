232d82bdb381489486aa4b531c44a09046072a21
PavelSapegin <pawsapegin@yandex.ru>
2025-11-02 13:48:16 +0300

    Error handling, formating and add comments

--- src/python_tasks/task7/curry.py
+++ src/python_tasks/task7/curry.py
@@ -1,28 +1,41 @@
-def curry(f, n):
-    def wrapper(*args):
-        if len(args) == n:
-            return f(*args)
+def curry(func, n):
+    """Функция каррирования"""
 
-        return lambda x: wrapper(*(args + (x,)))
+    def wrapper(*args):
+        if n <= 0 or not isinstance(n, int):
+            raise ValueError("Арность должна быть больше 0 и целым числом")
+        if len(args) > n:
+            raise ValueError("Арность дожна равняться количеству переданных аргументов")
+
+        if len(args) == n:  # Вызываем функцию, когда набрали нужное число аргументов
+            return func(*args)
+
+        return lambda x: wrapper(*(args + (x,)))  # Набираем аргументы
 
     return wrapper
 
 
 def uncurry(func, n):
+    """Функция антикаррирования"""
+
     def wrapper(*args):
-        temp_func = func
+        if len(args) > n:
+            raise ValueError(
+                "Арность должна равняться количеству переданных аргументов"
+            )
+            
+        if n <= 0 or not isinstance(n,int):
+            raise ValueError(
+                "Арность должна быть целым неотрицательным числом"
+            )
+        temp_func = func  # берем функцию без аргументов
         for i in args:
+            # накапливаем вызовы с постуающими аргументами
             temp_func = temp_func(i)
 
-        return temp_func
+        return temp_func # возвращаем функцию с накопленными аргументами
 
     return wrapper
 
-
-def sum3(x, y, z, w):
-    return x + y + z + w
-
-
-print(sum3)
 
 
