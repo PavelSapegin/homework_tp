4f51d7b7245f9ab421c397a70883c6f82bd137b4
PavelSapegin <pawsapegin@yandex.ru>
2025-11-22 16:24:12 +0300

    Add char analysis

--- src/python_tasks/task8/haffman.py
+++ src/python_tasks/task8/haffman.py
@@ -1,19 +1,30 @@
-class TreeNode():
-    def __init__(self,weight,left=None,right=None,symbol=None):
+from collections import Counter
+
+class TreeNode:
+    def __init__(self, weight, left=None, right=None, symbol=None):
         self.symbol = symbol
         self.weight = weight
         self.right = right
         self.left = left
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         return self.weight < other.weight
+
+
+string = "aaabbbccc"
+
+def char_analisys(s:str) -> dict[str, str]:
+    d = Counter(string)
+    LEN = len(string)
+    for k in d.keys():
+        d[k] /= LEN
+        
+    return d 
     
 
-d = {"a":0.43,"b":0.36,"c":0.21}
-
-
-def encode(msg:str) -> tuple[str, dict[str, str]]:
-    listnode = [TreeNode(v,symbol=k) for k,v in d.items()]
+def encode(msg: str) -> tuple[str, dict[str, str]]:
+    d = char_analisys(msg)
+    listnode = [TreeNode(v, symbol=k) for k, v in d.items()]
     listnode.sort()
 
     if len(listnode) == 0:
@@ -24,37 +35,42 @@ def encode(msg:str) -> tuple[str, dict[str, str]]:
         listnode.remove(l1)
         l2 = min(listnode)
         listnode.remove(l2)
-        
-        
-        new_node = TreeNode(weight=l1.weight+l2.weight,left=l1,right=l2)
+
+        new_node = TreeNode(weight=l1.weight + l2.weight, left=l1, right=l2)
         listnode.append(new_node)
         listnode.sort()
-        
-    
+
     # выдача кодов
 
     root = listnode[0]
     table = []
     bank = ""
-    
-    #Проверка на 1 символ
+
+    # Проверка на 1 символ
     if root.symbol is not None:
-        return [(root.symbol,"0")]
-    
-    def get_code(root,bank):
+        return [(root.symbol, "0")]
+
+    def get_code(root, bank):
         node = root
         if node.symbol is not None:
-            return table.append((node.symbol,bank))
-            
-        return get_code(node.left,bank+'0'), get_code(node.right,bank+'1')
-        
-    get_code(root,bank)
-        
-        
-        
-    return table
+            return table.append((node.symbol, bank))
+
+        return get_code(node.left, bank + "0"), get_code(node.right, bank + "1")
+
+    get_code(root, bank)
+
+    table = dict(table)
+
+    def transform(msg):
+        for symbol, code in table.items():
+            msg = msg.replace(symbol, code)
+
+        return msg
+
+    result_string = transform(msg)
+
+    return (result_string, table)
 
 
-#print(encode(d))
-res = encode(d)
+res = encode(string)
 print(res)
