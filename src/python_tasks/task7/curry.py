241fd949395f2320807186e5d8f828eca1da2417
PavelSapegin <pawsapegin@yandex.ru>
2025-12-26 15:39:59 +0300

    Refactoring code

--- src/python_tasks/task7/curry.py
+++ src/python_tasks/task7/curry.py
@@ -1,33 +1,37 @@
 def curry(func, n):
     """Функция каррирования"""
 
-    def wrapper(*args):
-        if n <= 0 or not isinstance(n, int):
+    if n <= 0 or not isinstance(n, int):
             raise ValueError("Арность должна быть больше 0 и целым числом")
-        if len(args) > n:
-            raise ValueError("Арность дожна равняться количеству переданных аргументов")
+        
+    def inner(accum_args):
+       
+        if len(accum_args) == n:  # Вызываем функцию, когда набрали нужное число аргументов
+            try:
+                return func(*accum_args)
+            except:
+                raise ValueError("Заданная арность n не соответствует фактической арности функции")
+        
+        return lambda x: inner(accum_args + (x,))  # Набираем аргументы
 
-        if len(args) == n:  # Вызываем функцию, когда набрали нужное число аргументов
-            return func(*args)
-
-        return lambda x: wrapper(*(args + (x,)))  # Набираем аргументы
-
-    return wrapper
+    return inner(())
 
 
 def uncurry(func, n):
     """Функция антикаррирования"""
 
+    if n <= 0 or not isinstance(n,int):
+            raise ValueError(
+                "Арность должна быть целым неотрицательным числом"
+            )
+            
     def wrapper(*args):
-        if len(args) > n:
+        if len(args) != n:
             raise ValueError(
                 "Арность должна равняться количеству переданных аргументов"
             )
             
-        if n <= 0 or not isinstance(n,int):
-            raise ValueError(
-                "Арность должна быть целым неотрицательным числом"
-            )
+        
         temp_func = func  # берем функцию без аргументов
         for i in args:
             # накапливаем вызовы с постуающими аргументами
@@ -37,5 +41,14 @@ def uncurry(func, n):
 
     return wrapper
 
+
+if __name__ == '__main__':
+    def sum3(x, y, z):
+        return x + y + z
+
+    sum3_curry = curry(sum3, 3)
+    sum3_uncurry = uncurry(sum3_curry, 3)
+    print(sum3_curry(1)(2)(3))    # 6
+    print(sum3_uncurry(1, 2, 3))  # 6 
 
 
