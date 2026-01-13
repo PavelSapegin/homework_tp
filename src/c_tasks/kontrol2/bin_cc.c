c74e79d1d38db8e592caff2ad53c1ddebfcef0a9
PavelSapegin <pawsapegin@yandex.ru>
2026-01-13 14:50:32 +0300

    Ref

--- src/c_tasks/kontrol2/bin_cc.c
+++ src/c_tasks/kontrol2/bin_cc.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 int srav(bool a[], bool b[], size_t len_a, size_t len_b)
 {
@@ -7,19 +7,15 @@ int srav(bool a[], bool b[], size_t len_a, size_t len_b)
     int idxa = len_a;
     int idxb = len_b;
 
-    for (int i = 0; i < len_a; ++i)
-    {
-        if (a[i] == 1)
-        {
+    for (int i = 0; i < len_a; ++i) {
+        if (a[i] == 1) {
             idxa = i;
             break;
         }
     }
 
-    for (int i = 0; i < len_b; ++i)
-    {
-        if (b[i] == 1)
-        {
+    for (int i = 0; i < len_b; ++i) {
+        if (b[i] == 1) {
             idxb = i;
             break;
         }
@@ -28,23 +24,16 @@ int srav(bool a[], bool b[], size_t len_a, size_t len_b)
     size_t true_len_a = len_a - idxa;
     size_t true_len_b = len_b - idxb;
 
-    if (true_len_a > true_len_b)
-    {
+    if (true_len_a > true_len_b) {
         return 1;
-    }
-    else if (true_len_a < true_len_b)
-    {
+    } else if (true_len_a < true_len_b) {
         return 2;
     }
 
-    for (int i = idxa; i < len_a; ++i)
-    {
-        if (a[i] > b[i])
-        {
+    for (int i = idxa; i < len_a; ++i) {
+        if (a[i] > b[i]) {
             return 1;
-        }
-        else if (a[i] < b[i])
-        {
+        } else if (a[i] < b[i]) {
             return 2;
         }
     }
@@ -54,15 +43,15 @@ int srav(bool a[], bool b[], size_t len_a, size_t len_b)
 
 int main()
 {
-    //Пример
-    bool a[8] = {0, 0, 0, 0, 1, 0, 0, 0};
-    bool b[8] = {0, 0, 1, 0, 0, 0, 0, 0};
-    res = srav(a,b,8,8);
+    // Пример
+    bool a[8] = { 0, 0, 0, 0, 1, 0, 0, 0 };
+    bool b[8] = { 0, 0, 1, 0, 0, 0, 0, 0 };
+    res = srav(a, b, 8, 8);
     if (res == 1)
         printf("Первое число больше");
     else if (res == 2)
         printf("Второе число больше");
-    
+
     else if (res == 3)
         printf("Числа равны");
 
